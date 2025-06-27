4f32333deb1f5e9b3eba8cbe4ab55ad1addd4768
worstmageplayer <shawnjun05@gmail.com>
2025-06-28 02:39:12 +1000

    haha

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -32,6 +32,20 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- === Commands === 
+vim.api.nvim_create_user_command("GitACP", function()
+  vim.ui.input({ prompt = "Commit message: " }, function(msg)
+    if not msg or msg == "" then
+      vim.notify("Aborted: no commit message", vim.log.levels.WARN)
+      return
+    end
+
+    vim.cmd("!git add .")
+    vim.cmd('!git commit -m "' .. msg:gsub('"', '\\"') .. '"')
+    vim.cmd("!git push")
+  end)
+end, { desc = "Git add . && commit && push" })
+
 -- File Explorer
 vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 
