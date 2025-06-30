dcc8f4d6dac093738ee94f55b4d489e3831cd2ed
worstmageplayer <shawnjun05@gmail.com>
2025-07-01 01:35:35 +1000

    lajsjdhfaslfasf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,7 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  },
-  config = function()
-    require('telescope').setup {
-        extensions = {
-            fzf = {}
-        }
-    }
-
-    require('telescope').load_extension('fzf')
-  end,
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
 }
