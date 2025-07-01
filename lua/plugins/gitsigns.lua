89a6dbe11e5c00d94e1bf6dad1efe67bd3782ae5
worstmageplayer <shawnjun05@gmail.com>
2025-07-01 21:35:15 +1000

    sdhfls

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,28 +1,24 @@
 return {
-    'lewis6991/gitsigns.nvim',
-    event = { "LazyFile" },
-    config = function()
-        require('gitsigns').setup {
-            signs = {
-                add          = { text = '▎' },
-                change       = { text = '▎' },
-                delete       = { text = '' },
-                topdelete    = { text = '' },
-                changedelete = { text = '▎' },
-                untracked    = { text = '▎' },
-            },
-            signcolumn = true,
-            numhl      = false,
-            linehl     = false,
-            word_diff  = false,
-            watch_gitdir = {
-                interval = 1000,
-                follow_files = true,
-            },
-            attach_to_untracked = true,
-            current_line_blame = false,
-            update_debounce = 100,
-            status_formatter = nil,
-        }
+  "lewis6991/gitsigns.nvim",
+  event = { 'BufReadPre', 'BufNewFile' },
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    signs_staged = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+    },
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
     end,
+  },
 }
