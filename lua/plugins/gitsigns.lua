69d97160cbc3f0026d19aff00aca6ba27f78f227
worstmageplayer <shawnjun05@gmail.com>
2025-07-01 21:27:34 +1000

    lasdfafniowef

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
     'lewis6991/gitsigns.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = { "LazyFile" }
     config = function()
         require('gitsigns').setup {
             signs = {
