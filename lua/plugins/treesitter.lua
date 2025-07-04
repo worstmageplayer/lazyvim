5264248c27f86c370d40f388479e49f60c14f883
worstmageplayer <shawnjun05@gmail.com>
2025-07-04 19:51:45 +1000

    even

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     require('nvim-treesitter.install').compilers = { 'zig' }
     require('nvim-treesitter.configs').setup {
