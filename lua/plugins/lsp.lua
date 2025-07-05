121cef7dca3e3148ef0c2be13edcffcd7caab6ad
worstmageplayer <shawnjun05@gmail.com>
2025-07-05 12:29:09 +1000

    commit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,7 @@ return {
       pyright = {},
       ts_ls = {},
       clangd = {},
+      rust_analyzer = {},
     }
   },
 
@@ -19,7 +20,7 @@ return {
       lspconfig[server].setup(config)
     end
 
-    require'lspconfig'.lua_ls.setup {
+    require('lspconfig').lua_ls.setup {
       on_init = function(client)
         local path = client.workspace_folders[1].name
         if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
@@ -41,6 +42,24 @@ return {
       settings = {
         Lua = {}
       }
+    }
+
+    require('lspconfig').rust_analyzer.setup {
+      capabilities = require('blink.cmp').get_lsp_capabilities(),
+      settings = {
+        ['rust-analyzer'] = {
+          check = {
+            command = 'clippy',
+          },
+          cargo = {
+            allFeatures = true,
+          },
+          procMacro = {
+            enable = true,
+          }
+        }
+      },
+      root_dir = require('lspconfig').util.root_pattern('Cargo.toml'),
     }
 
   end
